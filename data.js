f2a57e546c602008af85b1588e9c15589c2c7452
yxsj245 <17737475682@163.com>
2024-08-15 10:43:18 +0800

    main

--- data.js
+++ data.js
@@ -2,5 +2,9 @@
 const categories = {
     "Sky光遇": [
         { title: "如何安装国际服", file: "docs/Sky_file1.md" },
+    ],
+    "机场相关": [
+        { title: "付费好用的机场推荐", file: "docs/vpn_file1.md" },
+        { title: "自己搭建机场", file: "docs/vpn_file2.md" },
     ]
 };
